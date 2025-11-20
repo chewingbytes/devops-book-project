78f1ca12bb59b2e6fd71a1c0faaecdf3eff69024
chewingbytes <bryanchewzy24@gmail.com>
2025-11-20 15:25:30 +0800

    my work done

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,12 @@ import cors from "cors";
 
 import userRoutes from "./utils/jonathanUtils.js";
 import bookRoutes from "./utils/bryanUtil.js";
-import deleteRoutes from "./routes/deleteRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Grouping routes by the 3 people
-app.use("/api/users", userRoutes);        // Person 1
-app.use("/api/books", bookRoutes);        // Person 3
-app.use("/api/delete", deleteRoutes);     // Person 2 (Delete only)
+app.use("/api/users", userRoutes);
+app.use("/api/books", bookRoutes);
 
-app.listen(3000, () =>
-  console.log("Server running on http://localhost:3000")
-);
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
